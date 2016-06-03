8c8b6dab28e44877c07ae77c13e1bce06cde94b8
Luna Ruby <lkmnds@gmail.com>
2016-06-04 00:32:57 +1000

    Working contourns

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import numpy as np
 
 video_capture = cv2.VideoCapture(0)
 
+#video_capture.read()
 while True:
     # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.blur(gray, (3,3))
+    ret, im = video_capture.read()
+    imgray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 
-    edges = cv2.Canny(gray, 100, 200)
-    ret, thresh = cv2.threshold(edges, 127, 255, 0)
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    edges = cv2.Canny(imgray, 100, 200)
+    ret,thresh = cv2.threshold(edges,127,255,0)
+    img, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    cv2.drawContours(im2, contours, -1, (0,255,0), 3)
+    cv2.drawContours(im,contours,-1,(0,255,0),3)
 
     # Display the resulting frames
+    cv2.imshow('out', im)
     cv2.imshow('Canny', edges)
-    cv2.imshow('Contours', im2)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
